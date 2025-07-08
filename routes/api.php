8b300a64888046bb4ddafbedd17c671ad30c21ff
ibrahim şen <brhmsn80@gmail.com>
2025-07-08 06:46:24 +0300

    Fix missing semicolon in routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -11,4 +11,4 @@ Route::prefix('auth')->group(function() {
     Route::middleware('auth:sanctum')->group(function() {
         Route::post('/logout', [AuthController::class, 'logout']);
     });
-})
+});
