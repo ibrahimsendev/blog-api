ba2742aff07c5d683994b4934ab562e83663a037
ibrahim şen <brhmsn80@gmail.com>
2025-07-11 14:41:06 +0300

    Refactor minor adjustments in CommentRepositoryInterface

--- app/Interfaces/CommentRepositoryInterface.php
+++ app/Interfaces/CommentRepositoryInterface.php
@@ -8,8 +8,12 @@ use App\Models\Comment;
 interface CommentRepositoryInterface
 {
     public function all(): Collection;
+
     public function find(int $id): ?Comment;
+
     public function create(array $data): Comment;
+
     public function update(int $id, array $data): bool;
+    
     public function delete(int $id): bool;
 }
